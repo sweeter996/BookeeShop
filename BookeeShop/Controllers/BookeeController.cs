ac5d09efb602cd69213f1d1d2fe4d43521050d06
Nguyễn Ngọc Phương <1451010140Phuong@ou.edu.vn>
2018-07-14 01:51:46 +0700

    Update UI to Login, Register + Home

--- BookeeShop/Controllers/BookeeController.cs
+++ BookeeShop/Controllers/BookeeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BookeeShop.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -14,9 +15,17 @@ namespace BookeeShop.Controllers
             return View();
         }
 
+        [HttpGet]
         public ActionResult Login()
         {
+
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Class1 class1)
+        {
+            return RedirectToAction("Index","Bookee");
         }
 
         public ActionResult Register()
