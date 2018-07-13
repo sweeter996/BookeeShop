0b3f7a150001b7405f2169235a15619ab39a215b
Nguyễn Ngọc Phương <1451010140Phuong@ou.edu.vn>
2018-07-14 00:21:47 +0700

    Update UI FORM for FE

---
+++ BookeeShop/Controllers/BookeeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookeeShop.Controllers
+{
+    public class BookeeController : Controller
+    {
+        // GET: Bookee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+    }
+}
